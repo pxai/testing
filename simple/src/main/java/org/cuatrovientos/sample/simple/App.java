7894c1cc074c3cddd5db72f61662749eb7f27426
izenik <izenik@luser>
2015-03-15 13:51:01 +0100

    ok

---
+++ simple/src/main/java/org/cuatrovientos/sample/simple/App.java
@@ -0,0 +1,14 @@
+package org.cuatrovientos.sample.simple;
+
+/**
+ * Hello world!
+ * @author Pello Altadill Izura
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
